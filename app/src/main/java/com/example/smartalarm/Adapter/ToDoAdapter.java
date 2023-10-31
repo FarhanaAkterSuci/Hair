88ac9a49fc2787b186db97e7b17d134343466faa
FarhanaAkterSuci <b190305001@cse.jnu.ac.bd>
2023-11-01 01:13:09 +0600

    To do near to complete

--- app/src/main/java/com/example/smartalarm/Adapter/ToDoAdapter.java
+++ app/src/main/java/com/example/smartalarm/Adapter/ToDoAdapter.java
@@ -1,16 +1,21 @@
 package com.example.smartalarm.Adapter;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.smartalarm.AddNewTask;
 import com.example.smartalarm.Model.ToDoModel;
 import com.example.smartalarm.R;
 import com.example.smartalarm.ToDoSplash;
+import com.example.smartalarm.Utils.DatabaseHandler;
 
 import java.util.List;
 
@@ -18,8 +23,11 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
 
     private List<ToDoModel> todoList;
     private ToDoSplash activity;
-    public ToDoAdapter(ToDoSplash activity){
+    private DatabaseHandler db;
+    public ToDoAdapter(DatabaseHandler db ,ToDoSplash activity){
+
         this.activity = activity;
+        this.db = db;
     }
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
         View itemView = LayoutInflater.from(parent.getContext())
@@ -31,9 +39,22 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        db.openDatabase();
         ToDoModel item = todoList.get(position);
         holder.task.setText(item.getTask());
         holder.task.setChecked(toBoolean(item.getStatus()));
+        holder.task.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    db.updateStatus(item.getId(),1);
+                }
+                else {
+                    db.updateStatus(item.getId(),0);
+                }
+
+            }
+        });
     }
 
 
@@ -62,6 +83,27 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
 
 
 
+    }
+    public Context getContext(){
+        return activity;
+    }
+
+   public void deleteItem(int position){
+
+        ToDoModel item = todoList.get(position);
+        db.deleteTask(item.getId());
+        todoList.remove(position);
+        notifyItemRemoved(position);
+   }
+
+    public void editItem(int position){
+        ToDoModel item = todoList.get(position);
+        Bundle bundle = new Bundle();
+        bundle.putInt("id",item.getId());
+        bundle.putString("task", item.getTask());
+        AddNewTask fragment = new AddNewTask();
+        fragment.setArguments(bundle);
+        fragment.show(activity.getSupportFragmentManager(), AddNewTask.TAG);
     }
 
 
